60b96d09144b3925820ab96f7eb988485b3f963d
Niklas Tomas <niklas.thomas@hotmail.com>
2021-07-07 11:29:16 +0200

    update contitinals

--- 06_conditionals/main.go
+++ 06_conditionals/main.go
@@ -15,15 +15,36 @@ func main() {
 
 	}
 
+	peopleScore := map[string]int{"Joe": 20, "Doe": 50, "Sam": 77}
+	// if with initializer
+	if score, ok := peopleScore["Sam"]; ok {
+		fmt.Println(score)
+	}
+
 	// Switch
-	color := "blue"
+	color := "white"
 	switch color {
 	case "red":
 		fmt.Println("Color is red")
 	case "blue":
 		fmt.Println("Color is blue")
+	case "black", "white":
+		fmt.Println("Color is black or white")
 	default:
 		fmt.Println("Color is not red or blue")
+
+	}
+
+	var i interface{} = 1
+	switch i.(type) {
+	case int:
+		fmt.Println("i is and int")
+	case float64:
+		fmt.Println("i isn an float64")
+	case string:
+		fmt.Println("i is and string")
+	default:
+		fmt.Println("i is another type")
 
 	}
 
