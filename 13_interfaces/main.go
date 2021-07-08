8ddb1f891454c535839f3698f5d70c2696efa3be
Niklas Tomas <niklas.thomas@hotmail.com>
2021-07-08 12:51:45 +0200

    update interfaces

--- 13_interfaces/main.go
+++ 13_interfaces/main.go
@@ -5,6 +5,8 @@ import (
 	"math"
 )
 
+// Interface describs behavior
+
 type Shape interface {
 	area() float64
 }
@@ -29,11 +31,39 @@ func getArea(s Shape) float64 {
 	return s.area()
 }
 
+type Incrementer interface {
+	Increment() int
+}
+
+type IntCounter int
+
+func (ic *IntCounter) Increment() int {
+	*ic++
+	return int(*ic)
+}
+
 func main() {
 	circle := Circle{x: 0, y: 0, radius: 5}
 	rectangle := Rectangle{width: 10, height: 5}
 
 	fmt.Printf("Circle Area: %f\n", getArea(circle))
 	fmt.Printf("Rectangle Area: %f\n", getArea(rectangle))
+
+	myInt := IntCounter(0)
+	var inc Incrementer = &myInt
+	for i := 0; i < 10; i++ {
+		fmt.Println(inc.Increment())
+	}
+
+	// type switch
+	var i interface{} = 1
+	switch i.(type) {
+	case int:
+		fmt.Println("i is an integer")
+	case string:
+		fmt.Println("i is an string")
+	default:
+		fmt.Println("Dont know what type i is")
+	}
 
 }
